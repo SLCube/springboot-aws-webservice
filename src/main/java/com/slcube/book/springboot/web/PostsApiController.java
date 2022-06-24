01bbad70eb1a01c5d2539229cd554d7e94b3c827
SLCube <kgm0096@naver.com>
2022-06-24 23:30:21 +0900

    jpa를 이용해 게시글 등록/수정/조회 api구현 및 테스트코드 작성

---
+++ src/main/java/com/slcube/book/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.slcube.book.springboot.web;
+
+import com.slcube.book.springboot.service.posts.PostsService;
+import com.slcube.book.springboot.web.dto.PostsResponseDto;
+import com.slcube.book.springboot.web.dto.PostsSaveRequestDto;
+import com.slcube.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping(value = "/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping(value = "/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping(value = "/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
